c925ec56ff46e18968ecb5cedf9b1de5b6fced8e
Angela M. Godoy S <godoysalguero@outlook.com>
2018-08-09 20:59:14 -0500

    condicionales con IF

--- archivo1.js
+++ archivo1.js
@@ -1,10 +1,9 @@
-/* VARIABLES */
+/* CONDICIONALES IF */
 
-const constante = 10; // Variable cuyo valor no cambia (no se puede sobrescribir)
-var texto = '10'; /* <- variable que contiene un string */
-var numero = 5; /* <- variable que contiene un tipo numero */
-numero = numero + 1; // el valor de la variable se incrementa en 1 (haro vale 6)
-numero++; // El valor se incrementa igual que el anterior
-numero--; // el valor se decrementa en 1
-alert(texto + numero); /* <- el '+' se utiliza para concatenar */
-console.log
+var numero = 10;
+
+if (numero > 5) {
+    console.log('La condición es verdadera ('+ numero +' es mayor que 5)');
+} else {
+    console.log('La condicion es falsa (5 NO es mayor que '+ numero +')');
+}
